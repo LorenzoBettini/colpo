dea0d80e2ee2aa0d71da0b366e6b000943a9e802
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-07-04 17:12:19 +0200

    Added Trace

---
+++ colpo.core/src/main/java/colpo/core/semantics/Trace.java
@@ -0,0 +1,33 @@
+package colpo.core.semantics;
+
+/**
+ * @author Lorenzo Bettini
+ */
+public class Trace {
+
+	private StringBuilder builder = new StringBuilder();
+	private int indent = 0;
+
+	public void add(String string) {
+		builder.append(String.format("%s%s%s",
+			" ".repeat(indent), string, "\n"));
+	}
+
+	@Override
+	public String toString() {
+		return builder.toString();
+	}
+
+	public void addIndent() {
+		indent += 2;
+	}
+
+	public void removeIndent() {
+		indent -= 2;
+	}
+
+	public void reset() {
+		builder = new StringBuilder();
+		indent = 0;
+	}
+}
---
+++ colpo.core/src/test/java/colpo/core/semantics/TraceTest.java
@@ -0,0 +1,54 @@
+package colpo.core.semantics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TraceTest {
+
+	private Trace trace;
+	
+	@BeforeEach
+	void init() {
+		trace = new Trace();
+	}
+
+	@Test
+	void testTrace() {
+		trace.add("first line");
+		trace.add("second line");
+		assertEquals("""
+				first line
+				second line
+				""", trace.toString());
+	}
+
+	@Test
+	void testTraceIndent() {
+		trace.add("first line");
+		trace.addIndent();
+		trace.add("second line");
+		trace.addIndent();
+		trace.add("third line");
+		trace.removeIndent();
+		trace.add("fourth line");
+		trace.removeIndent();
+		trace.add("fifth line");
+		assertEquals("""
+				first line
+				  second line
+				    third line
+				  fourth line
+				fifth line
+				""", trace.toString());
+	}
+	@Test
+	void testTraceReset() {
+		trace.add("first line");
+		trace.addIndent();
+		trace.reset();
+		trace.add("second line");
+		assertEquals("second line\n", trace.toString());
+	}
+}
