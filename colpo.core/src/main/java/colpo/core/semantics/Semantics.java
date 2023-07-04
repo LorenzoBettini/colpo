42017492027d2ae612a7964a1d0b2018977d1f45
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-07-04 16:18:29 +0200

    test for attribute match in policy evaluation

--- colpo.core/src/main/java/colpo/core/semantics/Semantics.java
+++ colpo.core/src/main/java/colpo/core/semantics/Semantics.java
@@ -2,7 +2,10 @@ package colpo.core.semantics;
 
 import java.util.stream.IntStream;
 
+import colpo.core.AttributeMatcher;
 import colpo.core.ParticipantIndex;
+import colpo.core.ParticipantSuchThat;
+import colpo.core.ParticipantSuchThat.Quantifier;
 import colpo.core.Policies;
 import colpo.core.Policy;
 import colpo.core.Request;
@@ -15,15 +18,24 @@ import colpo.core.Rules;
 public class Semantics {
 
 	private Policies policies;
+	private AttributeMatcher matcher = new AttributeMatcher();
 
 	public Semantics(Policies policies) {
 		this.policies = policies;
 	}
 
 	public boolean evaluate(Request request) {
+		// REMEMBER: our indexes start from 1
 		var all = policies.all();
-		return IntStream.range(0, all.size())
-			// our indexes start from 1
+		var from = request.from();
+		// exclude the policy of the requester
+		var policyIndexes = IntStream.range(0, all.size())
+				.filter(i -> (i + 1) != request.requester().index());
+		if (from instanceof ParticipantSuchThat fromSuchThat && fromSuchThat.getQuantifier() == Quantifier.ALL) {
+			return policyIndexes
+				.allMatch(i -> evaluate(i + 1, all.get(i), request));
+		}
+		return policyIndexes
 			.anyMatch(i -> evaluate(i + 1, all.get(i), request));
 	}
 
@@ -32,6 +44,10 @@ public class Semantics {
 		if (from instanceof ParticipantIndex index && index.index() == i) {
 			// this check is only for requests generated during the evaluation
 			// of an exchange: users cannot specify an index for "from"
+			return evaluate(i, policy.rules(), request);
+		} else if (from instanceof ParticipantSuchThat fromSuchThat) {
+			if (!matcher.match(fromSuchThat.getAttributes(), policy.party()))
+				return false;
 			return evaluate(i, policy.rules(), request);
 		}
 		return false;
--- colpo.core/src/test/java/colpo/core/SemanticsTest.java
+++ colpo.core/src/test/java/colpo/core/SemanticsTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import colpo.core.ParticipantSuchThat.Quantifier;
 import colpo.core.semantics.Semantics;
 
 class SemanticsTest {
@@ -53,4 +54,67 @@ class SemanticsTest {
 		));
 	}
 
+	@Test
+	void shouldCheckAttributesMatch() {
+		policies.add(
+			new Policy( // index 1
+				new Attributes()
+					.add("name", "Alice"),
+				new Rules()
+					.add(new Rule(() -> false))))
+		.add(
+			new Policy( // index 2
+				new Attributes()
+					.add("role", "Provider")
+					.add("resource/name", "aResource"),
+				new Rules()
+					.add(new Rule(() -> true))))
+		.add(
+			new Policy( // index 2
+				new Attributes()
+					.add("name", "Bob")
+					.add("role", "Provider")
+					.add("resource/name", "aResource"),
+				new Rules()
+					.add(new Rule(() -> true))));
+		// Alice requests
+		// ( resource: (resource/name : "aResource"), from: anySuchThat (name : "Bob"))
+		assertTrue(semantics.evaluate(
+			new Request(
+				new ParticipantIndex(1), // Alice
+				new Attributes()
+					.add("resource", "aResource"),
+				new ParticipantSuchThat(Quantifier.ANY,
+						new Attributes()
+							.add("name", "Bob"))
+			)
+		));
+		// Alice requests
+		// ( resource: (resource/name : "aResource"), from: allSuchThat (role : "Provider"))
+		// NOTE: Alice's policy is not evaluated since she's the requester
+		// her attributes would not match the request
+		assertTrue(semantics.evaluate(
+			new Request(
+				new ParticipantIndex(1), // Alice
+				new Attributes()
+					.add("resource", "aResource"),
+				new ParticipantSuchThat(Quantifier.ALL,
+						new Attributes()
+							.add("role", "Provider"))
+			)
+		));
+		// Alice requests
+		// ( resource: (resource/name : "aResource"), from: allSuchThat (name : "Bob"))
+		// fails because there's only one Bob
+		assertTrue(semantics.evaluate(
+			new Request(
+				new ParticipantIndex(1), // Alice
+				new Attributes()
+					.add("resource", "aResource"),
+				new ParticipantSuchThat(Quantifier.ANY,
+						new Attributes()
+							.add("name", "Bob"))
+			)
+		));
+	}
 }
