a8fdbccbdd06bf70dfb1e50b410cd235b7cb4358
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-07-07 11:25:27 +0200

    enriched participant with me and requester

--- colpo.core/src/main/java/colpo/core/Participant.java
+++ colpo.core/src/main/java/colpo/core/Participant.java
@@ -6,15 +6,24 @@ package colpo.core;
 public class Participant {
 
 	private int index = -1;
-	private Quantifier quantifier;
+	private Quantifier quantifier = Quantifier.UNSET;
+	private Special special = Special.NOT_SPECIAL;
 	private Attributes attributes;
 
 	public enum Quantifier {
-		ANY, ALL
+		UNSET, ANY, ALL
+	}
+
+	public enum Special {
+		NOT_SPECIAL, ME, REQUESTER
 	}
 
 	private Participant(int index) {
 		this.index = index;
+	}
+
+	private Participant(Special special) {
+		this.special = special;
 	}
 
 	private Participant(Quantifier quantifier, Attributes attributes) {
@@ -24,6 +33,14 @@ public class Participant {
 
 	public static Participant index(int index) {
 		return new Participant(index);
+	}
+
+	public static Participant me() {
+		return new Participant(Special.ME);
+	}
+
+	public static Participant requester() {
+		return new Participant(Special.REQUESTER);
 	}
 
 	public static Participant anySuchThat(Attributes attributes) {
@@ -46,10 +63,20 @@ public class Participant {
 		return attributes;
 	}
 
+	public boolean isMe() {
+		return special == Special.ME;
+	}
+
+	public boolean isRequester() {
+		return special == Special.REQUESTER;
+	}
+
 	@Override
 	public String toString() {
 		if (index > 0)
 			return "" + index;
+		if (special != Special.NOT_SPECIAL)
+			return special.toString();
 		return String.format("%s: %s",
 			(quantifier == Quantifier.ANY ? "anySuchThat" : "allSuchThat"),
 			attributes
