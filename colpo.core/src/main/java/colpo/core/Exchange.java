9805e7223c89dee9af4d3aa7bc4830dc330ffece
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-07-07 10:18:09 +0200

    Introduced Exchange

---
+++ colpo.core/src/main/java/colpo/core/Exchange.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package colpo.core;
+
+/**
+ * @author Lorenzo Bettini
+ */
+public record Exchange(Participant from, Attributes resource, Participant to) {
+
+}
--- colpo.core/src/main/java/colpo/core/Rule.java
+++ colpo.core/src/main/java/colpo/core/Rule.java
@@ -5,17 +5,31 @@ package colpo.core;
  */
 public class Rule {
 	private ExpressionCode expression;
+	private Exchange exchange;
 
 	public Rule(ExpressionCode expression) {
 		this.expression = expression;
+	}
+
+	public Rule(ExpressionCode expression, Exchange exchange) {
+		this.expression = expression;
+		this.exchange = exchange;
 	}
 
 	public ExpressionCode getExpression() {
 		return expression;
 	}
 
+	public Exchange getExchange() {
+		return exchange;
+	}
+
 	@Override
 	public String toString() {
-		return "resource=" + expression.toString();
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("resource=");
+		stringBuilder.append(expression.toString());
+		stringBuilder.append((exchange != null ? "exchange=" + exchange.toString() : ""));
+		return stringBuilder.toString();
 	}
 }
