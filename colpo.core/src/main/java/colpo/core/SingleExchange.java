7f625f3884cd7a434926810b5bfbf138fd792955
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-10-17 15:07:40 +0200

    converted SingleExchange back to record

--- colpo.core/src/main/java/colpo/core/SingleExchange.java
+++ colpo.core/src/main/java/colpo/core/SingleExchange.java
@@ -6,41 +6,8 @@ package colpo.core;
 /**
  * @author Lorenzo Bettini
  */
-public class SingleExchange
+public record SingleExchange(Participant to, Attributes resource, Attributes credentials, Participant from)
 		implements Exchange {
-
-	private Participant from;
-	private Attributes resource;
-	private Attributes credentials;
-	private Participant to;
-
-	public SingleExchange(Participant to, Attributes resource, Attributes credentials, Participant from) {
-		this.from = from;
-		this.resource = resource;
-		this.credentials = credentials;
-		this.to = to;
-	}
-
-	
-	public Participant from() {
-		return from;
-	}
-
-
-	public Attributes resource() {
-		return resource;
-	}
-
-
-	public Attributes credentials() {
-		return credentials;
-	}
-
-
-	public Participant to() {
-		return to;
-	}
-
 
 	@Override
 	public String toString() {
