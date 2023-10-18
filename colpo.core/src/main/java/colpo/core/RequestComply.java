1e2d7a2e24d324e83a87ebb1d1b12eac8ff972de
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-10-18 14:13:15 +0200

    commented RequestComply

--- colpo.core/src/main/java/colpo/core/RequestComply.java
+++ colpo.core/src/main/java/colpo/core/RequestComply.java
@@ -1,9 +1,13 @@
 package colpo.core;
 
 /**
+ * Given a request created for an exchange and an existing request in the
+ * collected ones, the predicate has to decide whether the existing request can
+ * be considered compliant with the new one created for the exchange.
+ * 
  * @author Lorenzo Bettini
  */
 @FunctionalInterface
 public interface RequestComply {
-	boolean test(Request newRequest, Request existingRequest); // NOSONAR
+	boolean test(Request newRequest, Request existingRequest);
 }
