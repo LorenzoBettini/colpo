df3a547c33974b37dbabdd5b766d22ec0e925dcd
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2024-01-04 16:58:35 +0100

    updated fourth example (with custom compliance relation)

--- colpo.core/src/test/java/colpo/core/examples/CouriersExampleTest.java
+++ colpo.core/src/test/java/colpo/core/examples/CouriersExampleTest.java
@@ -12,7 +12,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import colpo.core.AndExchange;
+import colpo.core.AttributeMatcher;
 import colpo.core.Attributes;
+import colpo.core.ContextHandler;
 import colpo.core.ExpressionWithDescription;
 import colpo.core.OrExchange;
 import colpo.core.Policies;
@@ -330,6 +332,151 @@ class CouriersExampleTest {
 			          rule 3.1: condition true -> true
 			      result: true
 			    rule 2.1: END Exchange -> true
+			result: true
+			"""
+		);
+	}
+
+	@Test
+	void fourthScenario() {
+		policies.add(
+			new Policy( // index 1
+				new Attributes()
+					.add("service", "delivery")
+					.add("company", "RabbitService"),
+				new Rules()
+					.add(new Rule(
+						new Attributes()
+							.add("type", "addrInfo")
+							.add("city", "Lucca"),
+						new ExpressionWithDescription(
+							attributes -> attributes.name("company").equals("RabbitService"),
+							"company = RabbitService")))
+					.add(new Rule(
+						new Attributes()
+							.add("type", "addrInfo")
+							.add("city", "Lucca"),
+						new ExpressionWithDescription(
+							attributes -> !attributes.name("company").equals("RabbitService"),
+							"company != RabbitService"),
+						new SingleExchange(
+							me(),
+							new Attributes()
+								.add("type", "addrInfo")
+								.add("city", "Prato"),
+							requester())))
+					))
+		.add(
+			new Policy( // index 2
+				new Attributes()
+					.add("service", "delivery")
+					.add("company", "FastAndFurious"),
+				new Rules()
+					.add(new Rule(
+						new Attributes()
+							.add("type", "addrInfo")
+							.add("city", "Prato"),
+						new ExpressionWithDescription(
+							attributes -> {
+								var timeHour = (int) attributes.name("timeHour");
+								return timeHour > 7 && timeHour < 20 &&
+									attributes.name("position").equals("Prato");
+							},
+							"timeHour > 7 and timeHour < 20 and position = Prato"),
+						new SingleExchange(
+							me(),
+							new Attributes()
+								.add("type", "addrInfo")
+								.add("city", "Pisa"),
+							anySuchThat(new Attributes()
+									.add("service", "delivery")
+									.add("company", "RabbitService")))
+						))
+				))
+		.add(
+			new Policy( // index 3
+				new Attributes()
+					.add("service", "delivery")
+					.add("company", "RabbitService"),
+				new Rules()
+					.add(new Rule(
+						new Attributes()
+							.add("type", "addrInfo")
+							.add("city", "Grosseto")
+					))
+					.add(new Rule(
+						new Attributes()
+							.add("type", "addrInfo")
+							.add("city", "Pisa"),
+						new SingleExchange(
+							anySuchThat(new Attributes()
+									.add("service", "delivery")
+									.add("company", "RabbitService")),
+							new Attributes()
+								.add("type", "addrInfo"),
+							requester())
+					))
+				));
+		// don't assert policies for simplicity and readability
+
+		// add environmental information to context handler for the first two parties
+		semantics.contextHandler(new ContextHandler()
+			.add(1, "timeHour", 10)
+			.add(1, "position", "Prato")
+			.add(2, "timeHour", 10)
+			.add(2, "position", "Prato")
+			.add(3, "timeHour", 10)
+			.add(3, "position", "Pisa")
+		);
+
+		var attributesMatcher = new AttributeMatcher();
+		// custom compliance relation
+		semantics.requestComply((newRequest, existingRequest) -> 
+			newRequest.requester().equals(existingRequest.requester())
+			&& newRequest.from().equals(existingRequest.from())
+			&& attributesMatcher.match(newRequest.resource(), existingRequest.resource())
+		);
+
+		assertResultTrue(
+			new Request(
+				index(1), // RabbitService
+				new Attributes()
+					.add("type", "addrInfo")
+					.add("city", "Prato"),
+				anySuchThat(new Attributes()
+					.add("service", "delivery")
+					.add("company", "FastAndFurious"))
+			),
+			"""
+			evaluating Request[requester=1, resource=[(type : addrInfo), (city : Prato)], from=anySuchThat: [(service : delivery), (company : FastAndFurious)]]
+			  finding matching policies
+			    policy 2: from match([(service : delivery), (company : FastAndFurious)], [(service : delivery), (company : FastAndFurious)]) -> true
+			    policy 3: from match([(service : delivery), (company : FastAndFurious)], [(service : delivery), (company : RabbitService)]) -> false
+			  policy 2: evaluating Request[requester=1, resource=[(type : addrInfo), (city : Prato)], from=2]
+			    rule 2.1: resource match([(type : addrInfo), (city : Prato)], [(type : addrInfo), (city : Prato)]) -> true
+			    rule 2.1: condition timeHour > 7 and timeHour < 20 and position = Prato -> true
+			    rule 2.1: evaluating Exchange[to=ME, resource=[(type : addrInfo), (city : Pisa)], from=anySuchThat: [(service : delivery), (company : RabbitService)]]
+			    policy 1: from match([(service : delivery), (company : RabbitService)], [(service : delivery), (company : RabbitService)]) -> true
+			    policy 2: from match([(service : delivery), (company : RabbitService)], [(service : delivery), (company : FastAndFurious)]) -> false
+			    policy 3: from match([(service : delivery), (company : RabbitService)], [(service : delivery), (company : RabbitService)]) -> true
+			    evaluating Request[requester=2, resource=[(type : addrInfo), (city : Pisa)], from=1]
+			      policy 1: evaluating Request[requester=2, resource=[(type : addrInfo), (city : Pisa)], from=1]
+			        rule 1.1: resource match([(type : addrInfo), (city : Pisa)], [(type : addrInfo), (city : Lucca)]) -> false
+			      policy 1: evaluating Request[requester=2, resource=[(type : addrInfo), (city : Pisa)], from=1]
+			        rule 1.2: resource match([(type : addrInfo), (city : Pisa)], [(type : addrInfo), (city : Lucca)]) -> false
+			    result: false
+			    evaluating Request[requester=2, resource=[(type : addrInfo), (city : Pisa)], from=3]
+			      policy 3: evaluating Request[requester=2, resource=[(type : addrInfo), (city : Pisa)], from=3]
+			        rule 3.1: resource match([(type : addrInfo), (city : Pisa)], [(type : addrInfo), (city : Grosseto)]) -> false
+			      policy 3: evaluating Request[requester=2, resource=[(type : addrInfo), (city : Pisa)], from=3]
+			        rule 3.2: resource match([(type : addrInfo), (city : Pisa)], [(type : addrInfo), (city : Pisa)]) -> true
+			        rule 3.2: condition true -> true
+			        rule 3.2: evaluating Exchange[to=anySuchThat: [(service : delivery), (company : RabbitService)], resource=[(type : addrInfo)], from=REQUESTER]
+			        policy 1: from match([(service : delivery), (company : RabbitService)], [(service : delivery), (company : RabbitService)]) -> true
+			        policy 2: from match([(service : delivery), (company : RabbitService)], [(service : delivery), (company : FastAndFurious)]) -> false
+			        policy 3: from match([(service : delivery), (company : RabbitService)], [(service : delivery), (company : RabbitService)]) -> true
+			        rule 3.2: compliant request found Request[requester=1, resource=[(type : addrInfo)], from=2]
+			    result: true
 			result: true
 			"""
 		);
